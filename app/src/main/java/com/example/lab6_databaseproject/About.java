22d6ca956812d480150b8dc620113f6ecbf83c4d
harleenksandhu <hsand052@uottawa.ca>
2023-11-08 17:46:17 -0500

    adding project

---
+++ app/src/main/java/com/example/lab6_databaseproject/About.java
@@ -0,0 +1,16 @@
+package com.example.lab6_databaseproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class About extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
